08e635dd75a44c0fb0c2b5dbd0f338b5578ad742
hlbvinh <103242417+hlbvinh@users.noreply.github.com>
2022-08-15 14:35:27 +0700

    Update build_pex.sh

--- ci/build_pex.sh
+++ ci/build_pex.sh
@@ -22,8 +22,6 @@ cat subdependencies_requirements >> /tmp/requirements.txt
 #touch ~/.pex/build
 #ls -lah ~/.pex/build || true
 
-sudo apt install python3.7
-
 # cache for max 1 year
 # TODO once we have a lock file for the dependencies and subdependencies
 # we could use the --intransitive option
