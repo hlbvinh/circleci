22a7d2e1ddf3c2aafbc134889f13f244d4615d72
hlbvinh <103242417+hlbvinh@users.noreply.github.com>
2022-08-15 18:05:12 +0700

    Update build_pex.sh

--- ci/build_pex.sh
+++ ci/build_pex.sh
@@ -27,6 +27,8 @@ sed -e "/ambi_utils/c\ambi_utils" \
 # the content of CircleCI cache
 cat subdependencies_requirements >> /tmp/requirements.txt
 
+cat setuptools==65.0.0 >> /tmp/requirements.txt
+
 cat /tmp/requirements.txt
 
 echo "### PEX BUILD DIR BEFORE BUILD ###"
